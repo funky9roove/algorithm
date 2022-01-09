bed2ca9d15dd47744d79f9f027e1c169d46c175e
funky9roove <87808841+funky9roove@users.noreply.github.com>
2022-01-09 21:01:49 +0900

    Create star triangle right align(no bottom blank).c

---
+++ commit file/star triangle right align(no bottom blank).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j;
+    scanf_s("%d", &n,sizeof(n));
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+    if (j != n + 1) printf("\n");
+    }
+    return 0;
+}
